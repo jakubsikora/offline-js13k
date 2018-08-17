cfab6410fca44bbfab1abf62a5b2748a2f40417d
Jakub Sikora <jakub.sikora@pure360.com>
2018-08-17 17:20:08 +0100

    initial clean

--- src/canvas.js
+++ src/canvas.js
@@ -1,12 +1,12 @@
 import {
-  C_H,
-  C_W } from './constants';
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH } from './constants';
 
 class Canvas {
   constructor() {
     this.instance = document.getElementById('canvas');
-    this.instance.width = C_W;
-    this.instance.height = C_H;
+    this.instance.width = CANVAS_HEIGHT;
+    this.instance.height = CANVAS_WIDTH;
   }
 }
 
--- src/level.js
+++
@@ -1,7 +0,0 @@
-class Level {
-  constructor() {
-    this.id = 0;
-  }
-}
-
-export default new Level();
